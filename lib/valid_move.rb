d06f2492ac4b6f869e42c9ae2648b7a6d5a455bc
Jonathan Lin <jlin8293@gmail.com>
2018-01-13 22:53:36 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,16 @@
+def valid_move?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
+    return true
+  elsif position_taken?(board, index)
+    return false
+  else
+    return false
+  end
+end
+
 def position taken?(board, index)
   if board[index] == "nil" || "" || " "
     return false
   else
     return true
   end
-  
